de14f9364723e83d0b729c450a89a6475ff8386e
Moelaal <medo.elaal@gmail.com>
2022-07-14 19:40:00 +0200

    songs app

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,78 +1,8 @@
-import { combineReducers, createStore } from 'redux';
-console.clear();
-
-/// create policy
-const createPolicy = (name, amount) => {
+// Action creator
+export const selectSong = song => {
+  // Return an action
   return {
-    type: 'CREATE_POLICY',
-    payload: {
-      name,
-      amount,
-    },
+    type: 'SONG_SELECTED',
+    payload: song
   };
 };
-
-// deleting the policy
-const deletePolicy = (name) => {
-  return {
-    type: 'DELETE_POLICY',
-    payload: {
-      name,
-    },
-  };
-};
-
-const createClaim = (name, receivedMoney) => {
-  return {
-    type: 'CREATE_CLAIM',
-    payload: {
-      name,
-      receivedMoney,
-    },
-  };
-};
-
-// create Reducers
-
-const claimHistory = (state = [], action) => {
-  if (action.type === 'CREATE_CLAIM') {
-    return [...state, action.payload];
-  }
-  return state;
-};
-
-const accounting = (cashIn = 100, action) => {
-  if (action.type === 'CREATE_CLAIM') {
-    return cashIn - action.payload.receivedMoney;
-  } else if (action.type === 'CREATE_POLICY') {
-    return cashIn + action.payload.amount;
-  }
-  return cashIn;
-};
-
-const policies = (state = [], action) => {
-  if (action.type === 'CREATE_POLICY') {
-    return [...state, action.payload.name];
-  } else if (action.type === 'DELETE_POLICY') {
-    return state.filter((name) => name !== action.payload.name);
-  }
-  return state;
-};
-
-// const { createStore, combineReducer } = Redux;
-// console.log(combineReducer);
-
-const ourDepartment = combineReducers({ claimHistory, accounting, policies });
-
-const store = createStore(ourDepartment);
-
-createPolicy('mohamed', 2000);
-createClaim('mohamed', 1000);
-deletePolicy('mohamed');
-store.dispatch(createPolicy('mohamed', 2000));
-store.dispatch(createPolicy('mohamed', 7000));
-store.dispatch(createClaim('mohamed', 1000));
-store.dispatch(createPolicy('shady', 3888));
-store.dispatch(deletePolicy('shady'));
-
-console.log(store.getState());
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../actions/index';
+
 const App = () => {
   return <div>App</div>;
 };
